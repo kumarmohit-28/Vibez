ce2db532db3f801a51681fd58701ba90029ca7ba
kumarmohit-28 <65715673+kumarmohit-28@users.noreply.github.com>
2022-12-20 18:43:52 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb+srv://kumar:import__cv2@cluster0.vxerr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://kumar:import__cv2@cluster0.vxerr.mongodb.net/?retryWrites=true&w=majority",
     secretOrKey: "secret",
     secretOrPrivateKey: 'secrate',
     JWT_KEY:'fffff'
